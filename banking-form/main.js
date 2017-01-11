736a2608df60090b91b0220b72d118551286ade0
Zak Spence <zakspence@gmail.com>
2017-01-11 14:57:37 -0600

    #state-selector fill machine working

--- banking-form/main.js
+++ banking-form/main.js
@@ -1,13 +1,14 @@
-var stateList = [AL, AK, AZ, AR, CA, CO, CT, DE, FL, GA, HI, ID, IL, IN, IA, KS, KY, LA, ME, MD, MA, MI, MN, MS, MO, MT, NE, NV, NH, NJ, NM, NY, NC, ND, OH, OK, OR, PA, RI, SC, SD, TN, TX, UT, VT, VA, WA, WV, WI, WY]
+var stateList = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
 var stateSelector = document.getElementById("state-selector" );
-for(var i= 0; i< 50; i++ ){
-	var now = stateList[i ];
+for(var i= 0; i < 50; i++ ){
+	var now= stateList[i ];
 	console.log(now );
-	var option = document.createElement("OPTION" );
+	var option= document.createElement("OPTION" );
 	console.log(option );
-	option.value = now
-	console.log(option.value );
+	option.value= now
+	option.text= now
+	console.log(option.value, option.text );
 	stateSelector.appendChild(option );
 	console.log(stateSelector.childNodes );
-})
+}
